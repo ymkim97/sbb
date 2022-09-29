7d59e9ec467a8b11839fb06fde33211aa6342884
ymkim-gram <rladudaud97@naver.com>
2022-09-29 19:00:50 +0900

    message

---
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,33 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.ManyToOne;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+}
